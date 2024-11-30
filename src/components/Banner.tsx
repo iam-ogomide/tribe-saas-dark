c17a7a851a525e5d0e1c708f63337d69989f6b33
iam-ogomide <ogomidesamuel10@gmail.com>
2024-11-30 02:26:53 +0100

    first commit

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,13 @@
-export const Banner = () => {
-  return null;
+export const Banner = () =>  {
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
+    <div className="container">
+      <p className="font-medium">
+      <span className="hidden sm:inline">Introducing a completely redesigned interface - {" "}</span>
+      <a href="#" className="underline underline-offset-4 font-medium">Explore the demo</a>
+       </p>
+      
+    </div>
+  </div>
+  )
 };
